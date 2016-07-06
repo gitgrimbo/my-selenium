29fe9c40f896b4a6321acd381416f9042e4ff98f
gitgrimbo <gitgrimbo@gmail.com>
2016-07-06 17:14:41 +0100

    Improve Selenium scripts

--- node/node-config.bat
+++ node/node-config.bat
@@ -1,14 +1,23 @@
-REM    Use this file to set node-specific config
+title selenium-node
 
-set MY_SEL_GRID_URL=http://192.168.1.80:4444/grid/register
+REM Run from selenium parent directory
+set GRID_URL=http://192.168.1.80:4444/grid/register
 
-REM    This sets a sample node config file.
-REM    Override this for each unique node.
-set MY_SEL_NODE_CONFIG=%~dp0Sample_NodeWebDriver.json
+set          FIREFOX_EXE=c:\apps\Mozilla Firefox\46\firefox.exe
+set        IE_DRIVER_EXE=tmp\IEDriverServer.exe
+set    CHROME_DRIVER_EXE=tmp\chromedriver.exe
+set        PHANTOMJS_EXE=c:\apps\phantomjs\2.1.1\bin\phantomjs.exe
+set     GECKO_DRIVER_EXE=tmp\geckodriver.exe
 
+set    CHROME_DRIVER_ARG=-Dwebdriver.chrome.driver="%CHROME_DRIVER_EXE%"
+set        IE_DRIVER_ARG=-Dwebdriver.ie.driver="%IE_DRIVER_EXE%"
+set   FIREFOX_DRIVER_ARG=-Dwebdriver.firefox.bin="%FIREFOX_EXE%"
+set PHANTOMJS_DRIVER_ARG=-Dphantomjs.binary.path="%PHANTOMJS_EXE%"
+set     GECKO_DRIVER_ARG=-Dwebdriver.gecko.driver="%GECKO_DRIVER_EXE%"
 
-set MY_SEL_IEDRIVER_LOCATION=tmp\IEDriverServer.exe
-set MY_SEL_CHROMEDRIVER_LOCATION=tmp\chromedriver.exe
+set  FIREFOX_PROFILE_ARG=-Dwebdriver.firefox.profile=ff46-selenium
 
-
-set MY_SEL_SERVER_LOCATION=tmp\selenium-server-standalone.jar
+REM set NODE_CONFIG=-nodeConfig %~dp0Sample_NodeWebDriver.json
+set NODE_CONFIG=-nodeConfig %~dp0Sample_NodeWebDriver.json
+set DRIVER_ARGS=%FIREFOX_DRIVER_ARG% %CHROME_DRIVER_ARG% %IE_DRIVER_ARG% %PHANTOMJS_DRIVER_ARG% %GECKO_DRIVER_ARG%
+set HUB_ARGS=-hub %GRID_URL%
--- node/start-selenium-node.bat
+++ node/start-selenium-node.bat
@@ -2,11 +2,4 @@ REM    Setup the customisable variables
 
 call %~dp0node-config.bat
 
-
-REM    Start the node
-
-set MY_SEL_DRIVER_ARGS=-Dwebdriver.ie.driver=%MY_SEL_IEDRIVER_LOCATION% -Dwebdriver.chrome.driver=%MY_SEL_CHROMEDRIVER_LOCATION%
-
-set MY_SEL_HUB_ARGS=-hub %MY_SEL_GRID_URL% %MY_SEL_DRIVER_ARGS%
-
-java -jar %MY_SEL_SERVER_LOCATION% -role node %MY_SEL_HUB_ARGS% -nodeConfig %MY_SEL_NODE_CONFIG%
+java -jar tmp\selenium-server-standalone.jar -role node %HUB_ARGS% %DRIVER_ARGS% %NODE_CONFIG%
